f3f5de4c53740eb415ea45be8ee4d7d328381b28
myblackbeanca <alet@myblackbean.ca>
2024-11-22 19:50:15 +0530

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate,ScrollRestoration } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -27,7 +27,6 @@ import Parenting from './pages/Resources/Guides/Parenting';
 const App = () => {
   return (
     <Router>
-      <ScrollRestoration />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-grow">
